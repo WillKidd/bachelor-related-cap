5ea6e2e6c38899bb5a876ab27f5953f181ce06bd
Jan Zimmermann <Janzoo1234@gmail.com>
2024-05-12 22:24:41 +0200

    finally working

--- s4hana-mock-service/srv/s4hana-service.js
+++ s4hana-mock-service/srv/s4hana-service.js
@@ -1,36 +1,33 @@
 const cds = require('@sap/cds')
 module.exports = function (){
-  this.on('submitOrder', async (req)=>{
+
+  this.on("submitOrder", async (req)=>{
     const {customerID, items} = req.data;
-    
-    const tx = cds.transaction(req);
+    console.log(items);
 
-    const customerEntity = await tx.run(SELECT.from('mock.test.s4hana.Customers').where({ID: customerID}));
+    const customer = await cds.run(SELECT.one.from('mock.test.s4hana.Customers').where({ID: customerID}));
 
-    if (!customerEntity){
+    if(!customer){
       throw new Error('Customer not found!');
     }
 
+    
     let totalAmount = 0;
     for (const item of items){
-      const productEntity = await tx.run(SELECT.from('mock.test.s4hana.Products').where({ID: item.productID}))
-
-      if (!productEntity){
-        throw new Error('Product not found!');
-      }
-      totalAmount+= productEntity.price * item.quantity;
+      const product = await cds.run(SELECT.one.from('mock.test.s4hana.Products').where({ID: item.productID}));
+      totalAmount+= product.price * item.quantity;
     }
 
-    const newOrder = {
+
+    const Order = {
       customerID: customerID,
-      orderDate: new Date(),
+      orderDate: new Date().toISOString(),
       totalAmount: totalAmount,
       status: 'Pending',
-      items:items
+      items: items
     };
 
-    const createOrder = await tx.run(INSERT.into('mock.test.s4hana.Orders').entries(newOrder));
-    await tx.commit();
-    return createOrder;
-  })
+    const createOrder = await cds.run(INSERT.into('mock.test.s4hana.Orders').entries(Order));
+    }
+  )
 }
