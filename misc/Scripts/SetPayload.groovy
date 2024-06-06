54fe73a44585b70d6ce7eea053fd25e9d6bd4e75
Jan Zimmermann <Janzoo1234@gmail.com>
2024-06-06 11:35:00 +0200

    added groovy script for setting json payload and property entity

---
+++ misc/Scripts/SetPayload.groovy
@@ -0,0 +1,12 @@
+import com.sap.gateway.ip.core.customdev.util.Message;
+import java.util.HashMap;
+import groovy.json.JsonSlurper;
+import groovy.json.JsonOutput
+
+def Message processData(Message message) {
+    def json = message.getBody(java.io.Reader);
+    def data = new JsonSlurper().parse(json);
+    def jsonStr = JsonOutput.toJson(data.payload);
+    message.setBody(jsonStr);
+    return message;
+}
