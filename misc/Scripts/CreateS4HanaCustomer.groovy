1f000f426798e0a1d2e6b37e207aeaca7c15c9b7
Jan Zimmermann <Janzoo1234@gmail.com>
2024-06-06 11:50:27 +0200

    added script for creating salesforce customer

---
+++ misc/Scripts/CreateS4HanaCustomer.groovy
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import groovy.json.JsonSlurper;
+import groovy.json.JsonOutput;
+
+def Message processData(Message message) {
+    def json = message.getBody(java.io.Reader);
+    def data = new JsonSlurper().parse(json);
+    def customerJson = JsonOutput.toJson([
+        salesforceCustomerID : data.ID,
+        firstName            : data.firstName,
+        lastName             : data.lastName,
+        email                : data.email,
+        phone                : data.phone,
+        billingAddress       : data.billingAddress_ID
+    ])
+    message.setBody(customerJson);
+    return message;
+}
